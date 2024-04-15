327b892e7cebcde937cbd374bfc2f9f0a8d9e8a1
Maulana Daffa A <maulanada25@gmail.com>
2024-04-15 16:29:47 +0700

    fix: interceptor

--- src/main/java/com/techno/fromKt/config/PathMatchingInterceptor.java
+++ src/main/java/com/techno/fromKt/config/PathMatchingInterceptor.java
@@ -1,9 +1,11 @@
 package com.techno.fromKt.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class PathMatchingInterceptor implements WebMvcConfigurer {
     private AuthInterceptor authInterceptor;
 
