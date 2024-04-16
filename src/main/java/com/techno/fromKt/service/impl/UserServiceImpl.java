1c1b655cf6015489cfa646dd10a4f81738331d78
Maulana Daffa A <maulanada25@gmail.com>
2024-04-16 09:28:36 +0700

    fix: UserService

--- src/main/java/com/techno/fromKt/service/impl/UserServiceImpl.java
+++ src/main/java/com/techno/fromKt/service/impl/UserServiceImpl.java
@@ -3,16 +3,15 @@ package com.techno.fromKt.service.impl;
 import com.techno.fromKt.domain.dto.request.ReqUserDto;
 import com.techno.fromKt.domain.dto.response.ResMessageDto;
 import com.techno.fromKt.domain.dto.response.ResUserDto;
-import com.techno.fromKt.domain.entity.TypeEntity;
 import com.techno.fromKt.domain.entity.UserEntity;
 import com.techno.fromKt.repository.TypeRepository;
 import com.techno.fromKt.repository.UserRepository;
 import com.techno.fromKt.service.UserService;
+import com.techno.fromKt.util.GeneralFunction;
 import com.techno.fromKt.util.JwtGenerator;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import lombok.AllArgsConstructor;
-import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,6 +24,7 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
     UserRepository userRepository;
     TypeRepository typeRepository;
+    GeneralFunction generalFunction;
 
     private void checkDupeUsr(String username) {
         UserEntity usedUsername = userRepository.findByUsername(username);
@@ -62,20 +62,6 @@ public class UserServiceImpl implements UserService {
         }
         return user.get();
     }
-    private String typeAssign(String type) {
-        TypeEntity typeFound = typeRepository.findByName(nullType(type));
-        if (typeFound == null) {
-            throw new RuntimeException("Type not found"); // Replace RuntimeException with DataNotFoundException if it's defined in your project
-        }
-        return typeFound.getId(); // Assuming 'getTypeId' returns a String. Make sure this is non-null or handled if it can be.
-    }
-    private String nullType(String type) {
-        if (type.isEmpty()) {
-            return "Free";
-        } else {
-            return type;
-        }
-    }
     private String hashPw(String pw){
         Argon2 password = Argon2Factory.create();
         return password.hash(10, 65536 , 1, pw.toCharArray());
@@ -90,7 +76,7 @@ public class UserServiceImpl implements UserService {
                 .username(req.getUsername())
                 .email(req.getEmail())
                 .password(hashPw(req.getPassword()))
-                .type(typeAssign(req.getType()))
+                .type(generalFunction.typeAssign(req.getType()).getId())
                 .build();
         userRepository.save(input);
         return new ResMessageDto<>(
@@ -106,12 +92,11 @@ public class UserServiceImpl implements UserService {
             checkDupeUsr(req.getUsername());
         if (!user.getEmail().equals(req.getEmail()))
             checkDupeEml(req.getEmail());
-        Argon2 password = Argon2Factory.create();
         user.setName(req.getName());
         user.setUsername(req.getUsername());
         user.setEmail(req.getEmail());
         user.setPassword(hashPw(req.getPassword()));
-        user.setType(typeAssign(req.getType()));
+        user.setType(generalFunction.typeAssign(req.getType()).getId());
         userRepository.save(user);
         return new ResMessageDto<>(
                 200, "User added!",
